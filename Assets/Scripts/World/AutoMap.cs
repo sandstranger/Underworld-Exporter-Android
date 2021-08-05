0054f0a9bcdb71cc5f39ae6df8c121fd1b30b040
hankmorgan <aidanontour@yahoo.co.uk>
2021-08-05 23:05:47 +0100

    Fix to automap notes printing in bytes
    (cherry picked from commit 7bbe91e9a789c1133af38b44c7bbd52b14a7b614)

--- Assets/Scripts/World/AutoMap.cs
+++ Assets/Scripts/World/AutoMap.cs
@@ -237,7 +237,7 @@ public class AutoMap : Loader
             {
                 if ((lev_ark[automapNotesAddress + c].ToString() != "\0") && (!terminated))
                 {
-                    NoteText += lev_ark[automapNotesAddress + c];
+                    NoteText += (char)lev_ark[automapNotesAddress + c];
                 }
                 else
                 {
