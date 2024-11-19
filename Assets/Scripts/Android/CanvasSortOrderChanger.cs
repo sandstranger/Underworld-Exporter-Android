febc0e916ee6a611014df4c99b91358ad6619459
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-11-19 05:14:40 +0400

    fix sorting order

---
+++ Assets/Scripts/Android/CanvasSortOrderChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UnderworldExporter.Game
+{
+    public sealed class CanvasSortOrderChanger : MonoBehaviour
+    {
+        [SerializeField] private Canvas _canvas;
+        [SerializeField] private int _targetSortOrderValue;
+        private int _initialSortOrderValue;
+      
+        private void Awake() =>
+            _initialSortOrderValue = _canvas.sortingOrder;
+
+        public void ChangeSortOrder() =>
+            _canvas.sortingOrder = UWCharacter.Instance.MouseLookEnabled ? _targetSortOrderValue : _initialSortOrderValue;
+    }
+}
