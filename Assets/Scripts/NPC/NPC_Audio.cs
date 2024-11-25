45d1cedfd446a3f41e02e3970b027eb29cee65d6
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-11-25 04:34:10 +0400

    Revert "removing playing test sounds"
    This reverts commit 8cd5d96faf5351e380d72077027b37bc61410cd6.

--- Assets/Scripts/NPC/NPC_Audio.cs
+++ Assets/Scripts/NPC/NPC_Audio.cs
@@ -140,7 +140,6 @@ public class NPC_Audio : UWClass {
 
     public virtual void PlayIdleSound()
     {
-        return;
         playTestSound(this.npc.audVoice, Random.Range(0,6));// MusicController.SOUND_EFFECT_GUARDIAN_LAUGH_1);
     }
 
