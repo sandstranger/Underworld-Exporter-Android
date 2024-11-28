c7b9fe3702c9419222d8e815e7bcb0162a4fccb2
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-11-29 02:28:22 +0400

    fix mouse btn click

---
+++ Assets/Scripts/Android/ScreenControls/LeftMouseButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UnderworldExporter.Game
+{
+    public sealed class LeftMouseButton : MonoBehaviour
+    {
+        private readonly KeyCode _keyCode = KeyCode.Mouse0;
+        
+        public void OnPointerDown()
+        {
+            ScreenControlsManager.OnKeyDown(_keyCode);
+        }
+
+        public void OnPointerUp()
+        {
+            ScreenControlsManager.OnKeyUp(_keyCode);
+        }
+    }
+}
