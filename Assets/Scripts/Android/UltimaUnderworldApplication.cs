d3f875376063c3ead928b2e97bffa5862a3eb5bd
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-12-04 19:33:53 +0400

    added opportunity to writelogs

--- Assets/Scripts/Android/UltimaUnderworldApplication.cs
+++ Assets/Scripts/Android/UltimaUnderworldApplication.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using UnityEngine;
 
 namespace UnderworldExporter.Game
@@ -8,7 +9,7 @@ namespace UnderworldExporter.Game
         public static event Action<string> OnGamePathSet;
         public static event Action<string> OnMusicPathSet;
         public static PathMode PathMode;
-        
+
         private void Start()
         {
             AndroidUtils.RequestManageAllFilesAccess();
@@ -28,12 +29,97 @@ namespace UnderworldExporter.Game
 
         private void OnDestroy()
         {
+            InitLog();
             GameModel.CurrentModel.Save();
         }
 
         private void OnDisable()
         {
             GameModel.CurrentModel.Save();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void SubsystemInit() => InitLog();
+        
+        private sealed class LogHandler : ILogHandler, IDisposable
+        {
+            public static bool _loggingWasInit = false;
+            private readonly StreamWriter streamWriter;
+
+            public LogHandler()
+            {
+                var dataPath = GameModel.CurrentModel.BasePath;
+                
+                string filePath = Path.Combine(dataPath, "Player.log");
+
+                string errorMessage = null;
+                try
+                {
+                    if (File.Exists(filePath))
+                    {
+                        string prevPath = Path.Combine(dataPath, "Player-prev.log");
+                        File.Delete(prevPath);
+                        File.Move(filePath, prevPath);
+                    }
+                }
+                catch (Exception e)
+                {
+                    errorMessage = $"Could not preserve previous log: {e.Message}";
+                }
+
+                streamWriter = File.CreateText(filePath);
+                streamWriter.AutoFlush = true;
+
+                if (!string.IsNullOrEmpty(errorMessage))
+                {
+                    streamWriter.WriteLine(errorMessage);
+                }
+            }
+
+            public void LogException(Exception exception, UnityEngine.Object context)
+            {
+                streamWriter.WriteLine(exception.ToString());
+            }
+
+            public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+            {
+                string prefix = "";
+                switch (logType)
+                {
+                    case LogType.Error:
+                        prefix = "[Error] ";
+                        break;
+
+                    case LogType.Warning:
+                        prefix = "[Warning] ";
+                        break;
+
+                    case LogType.Assert:
+                        prefix = "[Assert] ";
+                        break;
+
+                    case LogType.Exception:
+                        prefix = "[Exception] ";
+                        break;
+                }
+
+                streamWriter.WriteLine(prefix + string.Format(format, args));
+            }
+
+            public void Dispose()
+            {
+                streamWriter.Close();
+            }
+        }
+
+        static void InitLog()
+        {
+            if (Application.isPlaying && Application.installMode != ApplicationInstallMode.Editor && 
+                !string.IsNullOrEmpty(GameModel.CurrentModel.BasePath) && !LogHandler._loggingWasInit)
+            {
+                LogHandler._loggingWasInit = true;
+                Debug.unityLogger.logHandler = new LogHandler();
+            }
         }
     }
 
